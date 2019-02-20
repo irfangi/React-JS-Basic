004caeaae887bb6c8eaa89678b8750b330915751
irfangi <irfangi564@gmail.com>
2019-02-20 17:00:12 +0700

    timer komponen

--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Timer
+// class Timer extends Component{
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       timer : props.mulai
+//     }
+//   }
+
+//   componentDidMount(){
+//     this.addInterval = setInterval(() => this.tambah(),1000)
+//   }
+
+//   componentWillMount(){
+//     clearInterval(this.addInterval)
+//   }
+//   tambah(){
+    
+//     this.setState((state,prop) => ({
+//       timer : parseInt(state.timer)+1
+//     }))
+//   }
+
+//   render(){
+//     return (
+//       <div>{this.state.timer } Detik</div>
+//     )
+//   }
+// }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/* <Timer mulai = "0"/>  timer*/}
         </header>
+        
       </div>
     );
   }
